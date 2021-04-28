f0d0c6fccb3548188f1c68d296f64b37458a65b2
Placid Masvidal <contact@placidmasvidal.com>
2021-04-28 13:53:37 +0200

    Database setup, Roles created

--- ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/ShopmeBackendApplication.java
+++ ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/ShopmeBackendApplication.java
@@ -2,8 +2,14 @@ package com.shopme.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan("com.shopme")
+@EnableJpaRepositories("com.shopme")
+@EntityScan({"com.shopme.common.entity", "com.shopme.admin.user"})
 public class ShopmeBackendApplication {
 
     public static void main(String[] args) {
