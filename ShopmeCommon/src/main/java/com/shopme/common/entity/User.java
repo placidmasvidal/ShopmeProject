701c5ed06b8e51b9326567f55b1248f3fa126b23
Placid Masvidal <contact@placidmasvidal.com>
2021-05-14 12:38:59 +0200

    Adding pending changes to last commit

--- ShopmeCommon/src/main/java/com/shopme/common/entity/User.java
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/User.java
@@ -29,7 +29,7 @@ public class User {
     @Column(length = 64)
     private String photos;
 
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
             name = "users_roles",
             joinColumns = @JoinColumn(name = "user_id"),
--- ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/security/ShopmeUserDetailsService.java
+++ ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/security/ShopmeUserDetailsService.java
@@ -1,2 +1,24 @@
-package com.shopme.admin.security;public class ShopmeUserDetailsService {
+package com.shopme.admin.security;
+
+import com.shopme.admin.user.UserRepository;
+import com.shopme.common.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class ShopmeUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.getUserByEmail(email);
+        if(user != null){
+            return new ShopmeUserDetails(user);
+        }
+
+        throw new UsernameNotFoundException("Could not find user with email: " + email);
+    }
 }
