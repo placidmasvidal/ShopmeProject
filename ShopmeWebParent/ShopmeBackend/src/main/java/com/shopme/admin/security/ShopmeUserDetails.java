43d756c00f6ff6f722d1cd4c8cba45292df2a052
Placid Masvidal <contact@placidmasvidal.com>
2021-05-14 14:06:43 +0200

    Added some thymeleaf extras dependencies for security

--- ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/security/ShopmeUserDetails.java
+++ ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/security/ShopmeUserDetails.java
@@ -58,4 +58,8 @@ public class ShopmeUserDetails implements UserDetails {
     public boolean isEnabled() {
         return user.isEnabled();
     }
+
+    public String getFullName(){
+        return this.user.getFirstName() + " " + this.user.getLastName();
+    }
 }
