1fbdbc9827304bbd8b1796239b231045229d0741
Placid Masvidal <contact@placidmasvidal.com>
2021-06-10 09:48:19 +0200

    Some refactors done

--- ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/config/MvcConfig.java
+++ ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/config/MvcConfig.java
@@ -12,31 +12,21 @@ public class MvcConfig implements WebMvcConfigurer {
 
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
-    String dirName = "user-photos";
-    Path userPhotosDir = Paths.get(dirName);
 
-    String userPhotosPath = userPhotosDir.toFile().getAbsolutePath();
+    exposeDirectory("user-photos", registry);
 
-    registry
-        .addResourceHandler("/" + dirName + "/**")
-        .addResourceLocations("file:" + userPhotosPath + "/");
+    exposeDirectory("../category-images", registry);
 
-    String categoryImagesDirName = "../category-images";
-    Path categoryImagesDir = Paths.get(categoryImagesDirName);
+    exposeDirectory("../brand-logos", registry);
 
-    String categoryImagesPath = categoryImagesDir.toFile().getAbsolutePath();
+  }
 
-    registry
-            .addResourceHandler("/category-images/**")
-            .addResourceLocations("file:" + categoryImagesPath + "/");
+  private void exposeDirectory(String pathPattern, ResourceHandlerRegistry registry){
+    Path path = Paths.get(pathPattern);
+    String absolutePath = path.toFile().getAbsolutePath();
+    String logicalPath = pathPattern.replace("../", "") + "/**";
 
-    String brandLogosDirName = "../brand-logos";
-    Path brandLogosDir = Paths.get(brandLogosDirName);
-
-    String brandLogosPath = brandLogosDir.toFile().getAbsolutePath();
-
-    registry
-            .addResourceHandler("/brand-logos/**")
-            .addResourceLocations("file:" + brandLogosPath + "/");
+    registry.addResourceHandler(logicalPath)
+    .addResourceLocations("file:" + absolutePath + "/");
   }
 }
