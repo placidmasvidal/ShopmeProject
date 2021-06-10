01e83ccb56437b9f11250ec916e5a28340f56c66
Placid Masvidal <contact@placidmasvidal.com>
2021-06-10 12:28:22 +0200

    ProductRepository Tests coded

--- ShopmeCommon/src/main/java/com/shopme/common/entity/Product.java
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/Product.java
@@ -197,4 +197,12 @@ public class Product {
     public void setBrand(Brand brand) {
         this.brand = brand;
     }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
