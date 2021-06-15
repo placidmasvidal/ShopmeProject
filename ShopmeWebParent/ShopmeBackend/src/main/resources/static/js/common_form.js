deeacb21192f4a61e17e353207670b39df62bab0
Placid Masvidal <contact@placidmasvidal.com>
2021-06-15 13:35:06 +0200

    Upload Extra Images for new Product implemented

--- ShopmeWebParent/ShopmeBackend/src/main/resources/static/js/common_form.js
+++ ShopmeWebParent/ShopmeBackend/src/main/resources/static/js/common_form.js
@@ -3,14 +3,10 @@ $(document).ready(function () {
         window.location = moduleURL;
     });
     $("#fileImage").change(function () {
-        fileSize = this.files[0].size;
-        if (fileSize > MAX_FILE_SIZE) {
-            this.setCustomValidity("You must choose an image less than " + MAX_FILE_SIZE + "bytes!");
-            this.reportValidity();
-        } else {
-            this.setCustomValidity("");
-            showImageThumbnail(this);
+        if(!checkFileSize(this)){
+            return;
         }
+        showImageThumbnail(this);
     });
 });
 
@@ -22,6 +18,20 @@ function showImageThumbnail(fileInput) {
     };
 
     reader.readAsDataURL(file);
+}
+
+function checkFileSize(fileInput){
+    fileSize = fileInput.files[0].size;
+    if (fileSize > MAX_FILE_SIZE) {
+        fileInput.setCustomValidity("You must choose an image less than " + MAX_FILE_SIZE + "bytes!");
+        fileInput.reportValidity();
+
+        return false;
+    } else {
+        fileInput.setCustomValidity("");
+
+        return true;
+    }
 }
 
 function showModalDialog(title, message) {
