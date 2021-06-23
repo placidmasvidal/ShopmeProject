e7d549dfcfeec4d7086a14e44d540089fbb975d7
Placid Masvidal <contact@placidmasvidal.com>
2021-06-23 16:11:11 +0200

    Issue category-images not loaded fixed
    Solved changing working directory for ShopmeFrontend to the module directory itselfs.

--- ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/config/MvcConfig.java
+++ ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/config/MvcConfig.java
@@ -1,5 +1,7 @@
 package com.shopme.admin.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,6 +11,8 @@ import java.nio.file.Paths;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
+
+  private static final Logger LOG = LoggerFactory.getLogger(MvcConfig.class);
 
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -24,6 +28,10 @@ public class MvcConfig implements WebMvcConfigurer {
     Path path = Paths.get(pathPattern);
     String absolutePath = path.toFile().getAbsolutePath();
     String logicalPath = pathPattern.replace("../", "") + "/**";
+
+    LOG.info("Backend PATH: {}", path);
+    LOG.info("Backend ABSOLUTE PATH: {}", absolutePath);
+    LOG.info("Backend LOGICAL PATH: {}", logicalPath);
 
     registry.addResourceHandler(logicalPath)
     .addResourceLocations("file:" + absolutePath + "/");
--- ShopmeWebParent/ShopmeFrontend/src/main/java/com/shopme/config/MvcConfig.java
+++ ShopmeWebParent/ShopmeFrontend/src/main/java/com/shopme/config/MvcConfig.java
@@ -3,12 +3,16 @@ package com.shopme.config;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
+
+    private static final Logger LOG = LoggerFactory.getLogger(MvcConfig.class);
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -19,10 +23,16 @@ public class MvcConfig implements WebMvcConfigurer {
 
     }
 
+//  Backend ABSOLUTE PATH: /home/placidmasvidal/git/repositories/ShopmeProject/ShopmeWebParent/ShopmeBackend/../category-images
+
     private void exposeDirectory(String pathPattern, ResourceHandlerRegistry registry){
         Path path = Paths.get(pathPattern);
         String absolutePath = path.toFile().getAbsolutePath();
         String logicalPath = pathPattern.replace("../", "") + "/**";
+
+        LOG.info("Frontend PATH: {}", path);
+        LOG.info("Frontend ABSOLUTE PATH: {}", absolutePath);
+        LOG.info("Frontend LOGICAL PATH: {}", logicalPath);
 
         registry.addResourceHandler(logicalPath)
                 .addResourceLocations("file:" + absolutePath + "/");
