40bb5a1fcfa9d3fe767594b6274e2673af8dd58d
Placid Masvidal <contact@placidmasvidal.com>
2021-06-29 12:57:46 +0200

    Implementing Settings Form, General Settings Tab implemented

--- ShopmeCommon/src/main/java/com/shopme/common/entity/Currency.java
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/Currency.java
@@ -63,11 +63,6 @@ public class Currency {
 
     @Override
     public String toString() {
-        return "Currency{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", symbol='" + symbol + '\'' +
-                ", code='" + code + '\'' +
-                '}';
+        return name + " - " + code + " - " + symbol;
     }
 }
