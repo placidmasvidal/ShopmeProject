954cbcf3ca170321bad2caaa16f6ebe87631b1f5
Placid Masvidal <contact@placidmasvidal.com>
2021-07-03 18:43:43 +0200

    Implementing Countries tab in Settings Section

--- ShopmeWebParent/ShopmeBackend/src/main/resources/static/js/countries_setting.js
+++ ShopmeWebParent/ShopmeBackend/src/main/resources/static/js/countries_setting.js
@@ -1,33 +1,64 @@
 var buttonLoad;
 var dropDownCountry;
+var buttonAddCountry;
+var buttonUpdateCountry;
+var buttonDeleteCountry;
+var labelCountryName;
+var fieldCountryName;
+var fieldCountryCode;
 
-$(document).ready(function (){
-   buttonLoad = $("#buttonLoadCountries");
-   dropDownCountry = $("#dropDownCountries");
-   
-   buttonLoad.click(function (){
-       loadCountries();
-   });
+$(document).ready(function () {
+    buttonLoad = $("#buttonLoadCountries");
+    dropDownCountry = $("#dropDownCountries");
+    buttonAddCountry = $("#buttonAddCountry");
+    buttonUpdateCountry = $("#buttonUpdateCountry");
+    buttonDeleteCountry = $("#buttonDeleteCountry");
+    labelCountryName = $("#labelCountryName");
+    fieldCountryName = $("#fieldCountryName");
+    fieldCountryCode = $("#fieldCountryCode");
+
+    buttonLoad.click(function () {
+        loadCountries();
+    });
+
+    dropDownCountry.on("change", function () {
+        changeFormStateToSelectedCountry();
+    });
 });
+
+function changeFormStateToSelectedCountry(){
+    buttonAddCountry.prop("value", "New");
+    buttonUpdateCountry.prop("disabled", false);
+    buttonDeleteCountry.prop("disabled", false);
+
+    labelCountryName.text("Selected Country:");
+
+    selectedCountryName = $("#dropDownCountries option:selected").text();
+    fieldCountryName.val(selectedCountryName);
+
+    countryCode = dropDownCountry.val().split("-")[1];
+    fieldCountryCode.val(countryCode);
+
+}
 
 function loadCountries() {
     url = contextPath + "countries/list";
-    $.get(url, function (responseJson){
+    $.get(url, function (responseJson) {
         dropDownCountry.empty();
 
-        $.each(responseJson, function (index, country){
-           optionValue = country.id + "-" + country.code;
-           $("<option>").val(optionValue).text(country.name).appendTo(dropDownCountry);
+        $.each(responseJson, function (index, country) {
+            optionValue = country.id + "-" + country.code;
+            $("<option>").val(optionValue).text(country.name).appendTo(dropDownCountry);
         });
-    }).done(function (){
+    }).done(function () {
         buttonLoad.val("Refresh Country List");
         showToastMessage("All countries have been loaded");
-    }).fail(function (){
+    }).fail(function () {
         showToastMessage("ERROR: Could not connect to server or server encountered an error.");
     });
 }
 
-function showToastMessage(message){
+function showToastMessage(message) {
     $("#toastMessage").text(message);
     $(".toast").toast('show');
 }
