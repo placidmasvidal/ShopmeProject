9ab382bdfdd35a85ed63710d1efb572c310e03e6
Placid Masvidal <contact@placidmasvidal.com>
2021-07-05 18:50:31 +0200

    CRUD operations for Country from settings form implemented

--- ShopmeWebParent/ShopmeBackend/src/main/resources/static/js/countries_setting.js
+++ ShopmeWebParent/ShopmeBackend/src/main/resources/static/js/countries_setting.js
@@ -32,7 +32,57 @@ $(document).ready(function () {
             changeFormStateToNew();
         }
     });
+
+    buttonUpdateCountry.click(function (){
+        updateCountry();
+    });
+
+    buttonDeleteCountry.click(function (){
+        deleteCountry();
+    });
 });
+
+function deleteCountry(){
+    optionValue = dropDownCountry.val();
+    countryId = optionValue.split("-")[0];
+    url = contextPath + "countries/delete/" + countryId;
+
+    $.get(url, function () {
+        $("#dropDownCountries option[value='" + optionValue + "']").remove();
+        changeFormStateToNew();
+    }).done(function () {
+        showToastMessage("The country has been deleted");
+    }).fail(function () {
+        showToastMessage("ERROR: Could not connect to server or server encountered an error.");
+    });
+}
+
+function updateCountry(){
+    url = contextPath + "countries/save";
+    countryName = fieldCountryName.val();
+    countryCode = fieldCountryCode.val();
+
+    countryId = dropDownCountry.val().split("-")[0];
+
+    jsonData = {id: countryId, name: countryName, code: countryCode};
+
+    $.ajax({
+        type: 'POST',
+        url: url,
+        beforeSend: function(xhr) {
+            xhr.setRequestHeader(csrfHeaderName, csrfValue);
+        },
+        data: JSON.stringify(jsonData),
+        contentType: 'application/json'
+    }).done(function (countryId){
+        $("#dropDownCountries option:selected").val(countryId + "-" + countryCode);
+        $("#dropDownCountries option:selected").text(countryName);
+        showToastMessage("The country has been updated");
+        changeFormStateToNew();
+    }).fail(function () {
+        showToastMessage("ERROR: Could not connect to server or server encountered an error.");
+    });
+}
 
 function addCountry(){
     url = contextPath + "countries/save";
@@ -51,6 +101,8 @@ function addCountry(){
     }).done(function (countryId){
         selectNewlyAddedCountry(countryId, countryCode, countryName);
         showToastMessage("The new country has been added");
+    }).fail(function () {
+        showToastMessage("ERROR: Could not connect to server or server encountered an error.");
     });
 }
 
