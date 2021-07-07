d3f63aeff4d70231df3a56a1039c13dda64fc665
Placid Masvidal <contact@placidmasvidal.com>
2021-07-07 22:11:58 +0200

    Dropdown menu for Settings tab in Navigation bar implemented

--- ShopmeWebParent/ShopmeBackend/src/main/resources/static/js/common.js
+++ ShopmeWebParent/ShopmeBackend/src/main/resources/static/js/common.js
@@ -5,7 +5,20 @@ $(document).ready(function () {
     });
 
     customizeDropDownMenu();
+    customizeTabs();
 });
+
+function customizeTabs(){
+    var url = document.location.toString();
+    if(url.match('#')){
+        $('.nav-tabs a[href="#' + url.split('#')[1] + '"]').tab('show');
+    }
+
+    // Change hash for page-reload
+    $('.nav-tabs a').on('shown.bs.tab', function (e){
+        window.location.hash = e.target.hash;
+    })
+}
 
 function customizeDropDownMenu() {
     $(".navbar .dropdown").hover(
