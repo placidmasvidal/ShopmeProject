b08a050fc94100b6a3a45260395032ab9f5adf19
Placid Masvidal <contact@placidmasvidal.com>
2021-07-07 21:17:35 +0200

    Some authorities granted

--- ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
+++ ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
@@ -42,7 +42,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.authorizeRequests()
-        .antMatchers("/users/**")
+        .antMatchers("/users/**", "/settings/**", "/countries/**", "/states/**")
         .hasAuthority("Admin")
         .antMatchers("/categories/**", "/brands/**")
         .hasAnyAuthority("Admin", "Editor")
