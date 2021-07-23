4b43380cd82b75b4606c1a5f57cafb02d3b61a52
Placid Masvidal <contact@placidmasvidal.com>
2021-07-23 15:13:23 +0200

    Some tests added

--- ShopmeWebParent/ShopmeBackend/src/test/java/com/shopme/admin/shippingrate/ShippingRateRepositoryTests.java
+++ ShopmeWebParent/ShopmeBackend/src/test/java/com/shopme/admin/shippingrate/ShippingRateRepositoryTests.java
@@ -6,7 +6,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.annotation.Rollback;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -15,7 +18,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 @Rollback(value = false)
 public class ShippingRateRepositoryTests {
 
-    @Autowired public ShippingRateRepository sut;
+    @Autowired private ShippingRateRepository sut;
+
+    @Autowired private TestEntityManager entityManager;
 
     @Test
     public void testCreateNew(){
@@ -32,6 +37,33 @@ public class ShippingRateRepositoryTests {
         assertThat(savedRate.getId()).isGreaterThan(0);
     }
 
+    @Test
+    public void testUpdate() {
+        Integer rateId = 1;
+        ShippingRate rate = entityManager.find(ShippingRate.class, rateId);
+        rate.setRate(9.15f);
+        rate.setDays(2);
+        ShippingRate updatedRate = sut.save(rate);
 
+        assertThat(updatedRate.getRate()).isEqualTo(9.15f);
+        assertThat(updatedRate.getDays()).isEqualTo(2);
+    }
+
+    @Test
+    public void testFindAll() {
+        List<ShippingRate> rates = (List<ShippingRate>) sut.findAll();
+        assertThat(rates.size()).isGreaterThan(0);
+
+        rates.forEach(System.out::println);
+    }
+
+    @Test
+    public void testDelete() {
+        Integer rateId = 1;
+        sut.deleteById(rateId);
+
+        ShippingRate rate = entityManager.find(ShippingRate.class, rateId);
+        assertThat(rate).isNull();
+    }
 
 }
