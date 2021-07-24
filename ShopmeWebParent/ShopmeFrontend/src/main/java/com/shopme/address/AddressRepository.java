9d3acc7f5807f084f98f03bf353bddf13685b929
Placid Masvidal <contact@placidmasvidal.com>
2021-07-24 09:33:16 +0200

    Address entity and repository created

---
+++ ShopmeWebParent/ShopmeFrontend/src/main/java/com/shopme/address/AddressRepository.java
@@ -0,0 +1,9 @@
+package com.shopme.address;
+
+import com.shopme.common.entity.Address;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AddressRepository extends CrudRepository<Address, Integer> {
+
+
+}
