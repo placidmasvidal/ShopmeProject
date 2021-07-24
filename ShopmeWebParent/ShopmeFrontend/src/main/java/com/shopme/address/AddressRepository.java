a6dfe9a379dab236b996aed550bead228b0109c3
Placid Masvidal <contact@placidmasvidal.com>
2021-07-24 09:43:35 +0200

    Some Address Repository methods and its tests coded

--- ShopmeWebParent/ShopmeFrontend/src/main/java/com/shopme/address/AddressRepository.java
+++ ShopmeWebParent/ShopmeFrontend/src/main/java/com/shopme/address/AddressRepository.java
@@ -1,9 +1,21 @@
 package com.shopme.address;
 
 import com.shopme.common.entity.Address;
+import com.shopme.common.entity.Customer;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 public interface AddressRepository extends CrudRepository<Address, Integer> {
 
+    List<Address> findByCustomer(Customer customer);
 
+    @Query("SELECT a FROM Address a WHERE a.id = ?1 AND a.customer.id = ?2")
+    Address findByIdAndCustomer(Integer addressId, Integer customerId);
+
+    @Query("DELETE FROM Address a WHERE a.id = ?1 AND a.customer.id = ?2")
+    @Modifying
+    void deleteByIdAndCustomer(Integer addressId, Integer customerId);
 }
--- ShopmeWebParent/ShopmeFrontend/src/test/java/com/shopme/address/AddressRepositoryTests.java
+++ ShopmeWebParent/ShopmeFrontend/src/test/java/com/shopme/address/AddressRepositoryTests.java
@@ -1,11 +1,18 @@
 package com.shopme.address;
 
+import com.shopme.common.entity.Address;
+import com.shopme.common.entity.Country;
+import com.shopme.common.entity.Customer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.annotation.Rollback;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
@@ -17,7 +24,67 @@ public class AddressRepositoryTests {
     @Autowired private TestEntityManager entityManager;
 
     @Test
-    public void testCreateAddress(){
+    public void testAddNew() {
+        Integer customerId = 5;
+        Integer countryId = 234; // USA
 
+        Address newAddress = new Address();
+        newAddress.setCustomer(new Customer(customerId));
+        newAddress.setCountry(new Country(countryId));
+        newAddress.setFirstName("Charles");
+        newAddress.setLastName("Brugger");
+        newAddress.setPhoneNumber("646-232-3902");
+        newAddress.setAddressLine1("204 Morningview Lane");
+        newAddress.setCity("New York");
+        newAddress.setState("New York");
+        newAddress.setPostalCode("10013");
+
+        Address savedAddress = sut.save(newAddress);
+
+        assertThat(savedAddress).isNotNull();
+        assertThat(savedAddress.getId()).isGreaterThan(0);
+    }
+
+    @Test
+    public void testFindByCustomer() {
+        Integer customerId = 5;
+        List<Address> listAddresses = sut.findByCustomer(new Customer(customerId));
+        assertThat(listAddresses.size()).isGreaterThan(0);
+
+        listAddresses.forEach(System.out::println);
+    }
+
+    @Test
+    public void testFindByIdAndCustomer() {
+        Integer addressId = 1;
+        Integer customerId = 5;
+
+        Address address = sut.findByIdAndCustomer(addressId, customerId);
+
+        assertThat(address).isNotNull();
+        System.out.println(address);
+    }
+
+    @Test
+    public void testUpdate() {
+        Integer addressId = 1;
+        String phoneNumber = "646-232-3932";
+
+        Address address = sut.findById(addressId).get();
+        address.setPhoneNumber(phoneNumber);
+
+        Address updatedAddress = sut.save(address);
+        assertThat(updatedAddress.getPhoneNumber()).isEqualTo(phoneNumber);
+    }
+
+    @Test
+    public void testDeleteByIdAndCustomer() {
+        Integer addressId = 1;
+        Integer customerId = 5;
+
+        sut.deleteByIdAndCustomer(addressId, customerId);
+
+        Address address = sut.findByIdAndCustomer(addressId, customerId);
+        assertThat(address).isNull();
     }
 }
