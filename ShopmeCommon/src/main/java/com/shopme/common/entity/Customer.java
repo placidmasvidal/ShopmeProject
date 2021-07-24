b0c00a32bab184f9d0ded72fd12137033d573ebd
Placid Masvidal <contact@placidmasvidal.com>
2021-07-24 10:30:07 +0200

    Show Primary Address in Address Book implemented

--- ShopmeCommon/src/main/java/com/shopme/common/entity/Customer.java
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/Customer.java
@@ -207,6 +207,30 @@ public class Customer {
     this.resetPasswordToken = resetPasswordToken;
   }
 
+  @Transient
+  public String getAddress(){
+    StringBuilder sb = new StringBuilder();
+    sb.append(firstName);
+
+    if(lastName != null && !lastName.isEmpty()) sb.append(" " + lastName);
+
+    if(!addressLine1.isEmpty()) sb.append(", " + addressLine1);
+
+    if(addressLine2 != null && !addressLine2.isEmpty()) sb.append(", " + addressLine2);
+
+    if(!city.isEmpty()) sb.append(", " + city);
+
+    if(state != null && !state.isEmpty()) sb.append(", " + state);
+
+    sb.append(", " + country.getName());
+
+    if(!postalCode.isEmpty()) sb.append(". Postal Code: " + postalCode);
+
+    if(!phoneNumber.isEmpty()) sb.append(". Phone Number: " + phoneNumber);
+
+    return sb.toString();
+  }
+
   @Override
   public String toString() {
     return "Customer{"
