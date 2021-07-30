5293806c6628824083f7f3f2ff552ecefac95ce2
Placid Masvidal <contact@placidmasvidal.com>
2021-07-30 12:26:10 +0200

    PayPal checkout functionality implemented but disabled and not tested
    Pending to have access to my sandbox developer paypal account

--- ShopmeWebParent/ShopmeFrontend/src/main/java/com/shopme/checkout/CheckoutInfo.java
+++ ShopmeWebParent/ShopmeFrontend/src/main/java/com/shopme/checkout/CheckoutInfo.java
@@ -1,5 +1,6 @@
 package com.shopme.checkout;
 
+import java.text.DecimalFormat;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -70,5 +71,10 @@ public class CheckoutInfo {
 
     public void setCodSupported(boolean codSupported) {
         this.codSupported = codSupported;
+    }
+
+    public String getPaymentTotal4PayPal(){
+        DecimalFormat formatter = new DecimalFormat("###,###.##");
+        return formatter.format(paymentTotal);
     }
 }
