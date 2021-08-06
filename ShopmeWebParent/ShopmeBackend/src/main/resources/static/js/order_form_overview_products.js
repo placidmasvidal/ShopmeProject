243717bb71bdffe06f935884ab77dded09a9251d
Placid Masvidal <contact@placidmasvidal.com>
2021-08-06 09:37:26 +0200

    Update subtotal when price and quantity changed in edit order form coded

--- ShopmeWebParent/ShopmeBackend/src/main/resources/static/js/order_form_overview_products.js
+++ ShopmeWebParent/ShopmeBackend/src/main/resources/static/js/order_form_overview_products.js
@@ -15,7 +15,36 @@ $(document).ready(function (){
     formatOrderAmounts();
     formatProductAmounts();
 
+    $("#productList").on("change", ".quantity-input", function (e){
+        updateSubtotalWhenQuantityChanged($(this));
+    });
+
+    $("#productList").on("change", ".price-input", function (e){
+        updateSubtotalWhenPriceChanged($(this));
+    });
 });
+
+function updateSubtotalWhenPriceChanged(input) {
+    let priceValue = input.val().replace(",", "");
+    let rowNumber = input.attr("rowNumber");
+    let quantityField = $("#quantity" + rowNumber);
+    let quantityValue = quantityField.val();
+    let newSubtotal = parseFloat(quantityValue) * parseFloat(priceValue);
+
+    let subtotalField = $("#subtotal" + rowNumber);
+    subtotalField.val($.number(newSubtotal, 2));
+}
+
+function updateSubtotalWhenQuantityChanged(input) {
+    let quantityValue = input.val();
+    let rowNumber = input.attr("rowNumber");
+    let priceField = $("#price" + rowNumber);
+    let priceValue = parseFloat(priceField.val().replace(",", ""));
+    let newSubtotal = parseFloat(quantityValue) * priceValue;
+
+    let subtotalField = $("#subtotal" + rowNumber);
+    subtotalField.val($.number(newSubtotal, 2));
+}
 
 function formatProductAmounts(){
     $(".cost-input").each(function (e) {
