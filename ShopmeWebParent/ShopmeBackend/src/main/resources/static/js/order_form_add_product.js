2d9d67ff9e1ea332f654a9db641337be8572e774
Placid Masvidal <contact@placidmasvidal.com>
2021-08-11 11:45:06 +0200

    Ajax call to rest method to get product info coded

--- ShopmeWebParent/ShopmeBackend/src/main/resources/static/js/order_form_add_product.js
+++ ShopmeWebParent/ShopmeBackend/src/main/resources/static/js/order_form_add_product.js
@@ -1,27 +1,27 @@
-$(document).ready(function (){
-    $("#products").on("click", "#linkAddProduct", function (e){
+$(document).ready(function () {
+    $("#products").on("click", "#linkAddProduct", function (e) {
         e.preventDefault();
         let link = $(this);
         let url = link.attr("href");
 
-        $("#addProductModal").on("shown.bs.modal", function (){
-           $(this).find("iframe").attr("src", url);
+        $("#addProductModal").on("shown.bs.modal", function () {
+            $(this).find("iframe").attr("src", url);
         });
 
         $("#addProductModal").modal();
     });
 });
 
-function addProduct(productId, productName){
+function addProduct(productId, productName) {
     getShippingCost(productId);
 }
 
-function getShippingCost(productId){
+function getShippingCost(productId) {
     let selectedCountry = $("#country option:selected");
     let countryId = selectedCountry.val();
 
     let state = $("#state").val();
-    if(state.length == 0){
+    if (state.length == 0) {
         state = $("#city").val();
     }
 
@@ -31,16 +31,27 @@ function getShippingCost(productId){
     $.ajax({
         type: 'POST',
         url: requestUrl,
-        beforeSend: function(xhr) {
+        beforeSend: function (xhr) {
             xhr.setRequestHeader(csrfHeaderName, csrfValue);
         },
         data: params
-    }).done(function (shippingCost){
-        alert("Shipping cost = " + shippingCost);
+    }).done(function (shippingCost) {
+        getProductInfo(productId);
     }).fail(function (err) {
         showWarningModal(err.responseJSON.message);
-    }).always(function(){
+        getProductInfo(productId);
+    }).always(function () {
         $("#addProductModal").modal("hide");
+    });
+}
+
+function getProductInfo(productId){
+    let requestURL = contextPath + "products/get/" + productId;
+
+    $.get(requestURL, function (productJson){
+        console.log(productJson);
+    }).fail(function(err){
+        showWarningModal(err.responseJSON.message);
     });
 }
 
@@ -50,7 +61,7 @@ function isProductAlreadyAdded(productId) {
     $(".hiddenProductId").each(function (e) {
         let aProductId = $(this).val();
 
-        if(aProductId == productId){
+        if (aProductId == productId) {
             productExists = true;
             return;
         }
