95c4f21adf540b593510cdde7d5e915ce40e2097
Placid Masvidal <contact@placidmasvidal.com>
2021-08-11 11:33:10 +0200

    Rest Controller method to get Product info from DTO coded

--- ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/product/controller/ProductRestController.java
+++ ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/product/controller/ProductRestController.java
@@ -1,23 +1,36 @@
 package com.shopme.admin.product.controller;
 
 import com.shopme.admin.product.ProductService;
+import com.shopme.common.entity.product.Product;
+import com.shopme.common.exception.ProductNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductRestController {
 
-    private ProductService productService;
+  private ProductService productService;
 
-    @Autowired
-    public ProductRestController(ProductService productService){
-        this.productService = productService;
-    }
+  @Autowired
+  public ProductRestController(ProductService productService) {
+    this.productService = productService;
+  }
 
-    @PostMapping("/products/check_unique")
-    public String checkUnique(Integer id, String name){
-        return productService.checkUnique(id, name);
-    }
+  @PostMapping("/products/check_unique")
+  public String checkUnique(Integer id, String name) {
+    return productService.checkUnique(id, name);
+  }
 
+  @GetMapping("/products/get/{id}")
+  public ProductDTO getProductInfo(@PathVariable("id") Integer id) throws ProductNotFoundException {
+    Product product = productService.get(id);
+    return new ProductDTO(
+        product.getName(),
+        product.getMainImagePath(),
+        product.getDiscountPrice(),
+        product.getCost());
+  }
 }
