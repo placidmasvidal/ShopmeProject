c3baf1b6e6caceaa3ec2126cb28e18efdd958684
Placid Masvidal <contact@placidmasvidal.com>
2021-08-25 12:20:12 +0200

    Success modal dialog for updated order status coded

--- ShopmeWebParent/ShopmeBackend/src/main/resources/static/js/orders_shipper.js
+++ ShopmeWebParent/ShopmeBackend/src/main/resources/static/js/orders_shipper.js
@@ -1,11 +1,14 @@
 var confirmText;
 var confirmModalDialog;
 var yesButton;
+var noButton;
 
 $(document).ready(function () {
     confirmText = $("#confirmText");
     confirmModalDialog = $("#confirmModal");
     yesButton = $("#yesButton");
+    noButton = $("#noButton");
+
     $(".linkUpdateStatus").on("click", function (e){
         e.preventDefault();
         let link = $(this);
@@ -32,13 +35,17 @@ function sendRequestToUpdateOrderStatus(button){
             xhr.setRequestHeader(csrfHeaderName, csrfValue);
         }
     }).done(function (response) {
+        showMessageModal("Order updated successfully");
         console.log(response);
     }).fail(function (err) {
-
+        showMessageModal("Error updating order status");
     });
 }
 
 function showUpdateConfirmModal(link) {
+    noButton.text("NO");
+    yesButton.show();
+
     let orderId = link.attr("orderId");
     let status = link.attr("status");
 
@@ -46,4 +53,11 @@ function showUpdateConfirmModal(link) {
 
     confirmText.text("Are you sure you want to update status of the order ID #" + orderId + " to " + status + "?");
     confirmModalDialog.modal();
+}
+
+function showMessageModal(message){
+    noButton.text("Close");
+    yesButton.hide();
+
+    confirmText.text(message);
 }
