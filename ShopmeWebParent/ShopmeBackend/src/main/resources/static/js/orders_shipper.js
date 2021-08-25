34a29e97020f570d633207f6004b35a6003e595c
Placid Masvidal <contact@placidmasvidal.com>
2021-08-25 11:41:15 +0200

    Confirmation dialog to update order status coded

--- ShopmeWebParent/ShopmeBackend/src/main/resources/static/js/orders_shipper.js
+++ ShopmeWebParent/ShopmeBackend/src/main/resources/static/js/orders_shipper.js
@@ -1,6 +1,20 @@
+var confirmText;
+var confirmModalDialog;
+
 $(document).ready(function () {
+    confirmText = $("#confirmText");
+    confirmModalDialog = $("#confirmModal");
     $(".linkUpdateStatus").on("click", function (e){
         e.preventDefault();
-        alert($(this).attr("href"));
+        let link = $(this);
+        showUpdateConfirmModal(link);
     });
 });
+
+function showUpdateConfirmModal(link) {
+    let orderId = link.attr("orderId");
+    let status = link.attr("status");
+
+    confirmText.text("Are you sure you want to update status of the order ID #" + orderId + " to " + status + "?");
+    confirmModalDialog.modal();
+}
