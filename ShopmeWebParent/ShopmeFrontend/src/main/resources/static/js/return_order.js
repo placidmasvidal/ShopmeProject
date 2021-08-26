b964c8000fd1377e43be2b5551f9879c2c100600
Placid Masvidal <contact@placidmasvidal.com>
2021-08-26 14:13:51 +0200

    Reason/note form in return modal dialog coded

--- ShopmeWebParent/ShopmeFrontend/src/main/resources/static/js/return_order.js
+++ ShopmeWebParent/ShopmeFrontend/src/main/resources/static/js/return_order.js
@@ -1,5 +1,6 @@
 const returnModal = $("#returnOrderModal");
 const modalTitle = $("#returnOrderModalTitle");
+const fieldNote = $("#returnNote");
 
 $(document).ready(function (){
     $(".linkReturnOrder").on("click", function (e){
@@ -12,4 +13,10 @@ function handleReturnOrderLink(link) {
     let orderId = link.attr("orderId");
     returnModal.modal("show");
     modalTitle.text("Return Order ID #" + orderId);
+}
+
+function submitReturnOrderForm(){
+    let reason = $("input[name='returnReason']:checked").val();
+    let note = fieldNote.val();
+    alert(reason + " - " + note);
 }
